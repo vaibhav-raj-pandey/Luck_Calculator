670d54cd42ba7b2ac732ef04af60bf271afdb0e0
VAIBHAV PANDEY <rajvaibhavpandey00@gmail.com>
2020-07-13 19:00:37 +0530

    Updates

---
+++ precache-manifest.eba0954479fb53188a6a0cb35a500b2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9ce4a2ca5ab37936628b2b581c53c9b",
+    "url": "/Luck_Calculator/index.html"
+  },
+  {
+    "revision": "23ac7df28fa0e94f3ca2",
+    "url": "/Luck_Calculator/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f832fdc72977b34dc5c5",
+    "url": "/Luck_Calculator/static/css/main.0a686a35.chunk.css"
+  },
+  {
+    "revision": "23ac7df28fa0e94f3ca2",
+    "url": "/Luck_Calculator/static/js/2.6df58e89.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Luck_Calculator/static/js/2.6df58e89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f832fdc72977b34dc5c5",
+    "url": "/Luck_Calculator/static/js/main.7e9a07d0.chunk.js"
+  },
+  {
+    "revision": "e020909df13a277a1520",
+    "url": "/Luck_Calculator/static/js/runtime-main.4517807d.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Luck_Calculator/precache-manifest.eba0954479fb53188a6a0cb35a500b2a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Luck_Calculator/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
